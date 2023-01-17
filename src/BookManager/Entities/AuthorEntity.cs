ebee53d77ca4ce0a92e9394e41de51c570ef7fc1
Ander De Abrisqueta Rangel <ander.deabrisqueta@gmail.com>
2023-01-17 18:57:01 +0100

    Created DBContext

---
+++ src/BookManager/Entities/AuthorEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace BookManager.Entities
+{
+    public class AuthorEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birth { get; set; }
+        public String CountryCode { get; set;}
+
+        // Navigation properties
+        public List<BookEntity> Books { get; set; } = new();
+    }
+}
---
+++ src/BookManager/Entities/BookEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace BookManager.Entities
+{
+    public class BookEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedOn { get; set; }
+        public int AuthorId { get; set; }
+
+       // Navigation properties
+        public AuthorEntity Author  { get; set; }
+        
+    }
+}
