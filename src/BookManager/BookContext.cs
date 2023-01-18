4aa9eb55bc7e9437c74273c302a812469d305031
Ander De Abrisqueta Rangel <ander.deabrisqueta@gmail.com>
2023-01-18 19:05:20 +0100

    OnConfiguring method

--- src/BookManager/BookContext.cs
+++ src/BookManager/BookContext.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookManager.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookManager
 {
     public class BookContext
         : DbContext
     {
+        private const string ConnectionString = "Server=localhost;Database=BookCore;user=sa;password=Lem0nCode!;Encrypt=False";
+
+        public DbSet<BookEntity> Book { get; set; }
+        public DbSet<AuthorEntity> Author { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
     }
 }
