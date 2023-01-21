d4516ca01c9afc243fd6a715fb1e677139d55b93
Ander De Abrisqueta Rangel <ander.deabrisqueta@gmail.com>
2023-01-21 19:26:27 +0100

    Migrations

--- src/BookManager/BookContext.cs
+++ src/BookManager/BookContext.cs
@@ -8,12 +8,22 @@ namespace BookManager
     {
         private const string ConnectionString = "Server=localhost;Database=BookCore;user=sa;password=Lem0nCode!;Encrypt=False";
 
-        public DbSet<BookEntity> Book { get; set; }
-        public DbSet<AuthorEntity> Author { get; set; }
+        public DbSet<BookEntity> Books { get; set; }
+        public DbSet<AuthorEntity> Authors { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<AuthorEntity>()
+                .HasMany(x => x.Books)
+                .WithOne(x => x.Author)
+                .HasForeignKey(x => x.AuthorId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
